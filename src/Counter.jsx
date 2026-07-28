e916975c33552873c5318d0bfd771e38528583b8
Safaa <safaaeltom@gmail.com>
2026-07-28 23:12:51 +0300

    feat: prevent counter from going below zero

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -7,7 +7,7 @@ const Counter = () => {
         <div>
             <h1>{count}</h1>
             <button onClick = {()=>setCount(count +1)}>Increase</button>
-            <button onClick = {()=>setCount(count -1)}>Decrease</button>
+            <button onClick = {()=>{if(count>0){setCount(count -1)}}}>Decrease</button>
             <button onClick = {()=>setCount(0)}>Reset</button>
         </div>
      );
