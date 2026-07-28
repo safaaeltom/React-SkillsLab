75ed18ebb633964f9573cbd6a144bab4e4563df5
Safaa <safaaeltom@gmail.com>
2026-07-28 21:22:17 +0300

    feat: build basic counter with increase, decrease, and reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Counter from'./Counter';
+
 function App() {
+  
   return (
     <div>
       <h1>React Skills</h1>
+      < Counter />
     </div>
   )
 }
---
+++ src/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    return ( 
+        <div>
+            <h1>{count}</h1>
+            <button onClick = {()=>setCount(count +1)}>Increase</button>
+            <button onClick = {()=>setCount(count -1)}>Decrease</button>
+            <button onClick = {()=>setCount(0)}>Reset</button>
+        </div>
+     );
+}
+ 
+export default Counter;
