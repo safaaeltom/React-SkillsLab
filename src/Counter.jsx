0bb01a84c6170a246a2be0e20e31cf43419491f3
Safaa <safaaeltom@gmail.com>
2026-07-29 09:02:50 +0300

    feat: prevent counter from going above 10

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -6,7 +6,7 @@ const Counter = () => {
     return ( 
         <div>
             <h1>{count}</h1>
-            <button onClick = {()=>setCount(count +1)}>Increase</button>
+            <button onClick = {()=>{if(count<10){setCount(count +1)}}}>Increase</button>
             <button onClick = {()=>{if(count>0){setCount(count -1)}}}>Decrease</button>
             <button onClick = {()=>setCount(0)}>Reset</button>
         </div>
