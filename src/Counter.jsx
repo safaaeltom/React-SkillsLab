882a577f5181520951266b481386874b2c6c9f1a
Safaa <safaaeltom@gmail.com>
2026-07-29 11:03:16 +0300

    feat: add conditional counter messages

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -9,6 +9,9 @@ const Counter = () => {
             <button onClick = {()=>{if(count<10){setCount(count +1)}}}>Increase</button>
             <button onClick = {()=>{if(count>0){setCount(count -1)}}}>Decrease</button>
             <button onClick = {()=>setCount(0)}>Reset</button>
+
+            {count===5 && <h3>Half way!</h3>}
+            {count===10 && <h3>Maximum reached!</h3>}
         </div>
      );
 }
