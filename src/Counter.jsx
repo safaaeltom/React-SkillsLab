a77a292b9cd13049f4af6374f61e840f56b318a5
Safaa <safaaeltom@gmail.com>
2026-07-30 11:11:14 +0300

    feat: link progress bar width to counter state

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
+import './Counter.css';
 
 const Counter = () => {
     const [count, setCount] = useState(0);
 
     return ( 
         <div>
-            
+            <div className="progress-container">
+                <div style={{ width: count*10 + "%" }} className="progress-bar"></div>
+            </div>
             <h1>{count*10}%</h1>
             <button onClick = {()=>{if(count<10){setCount(count +1)}}}>Increase</button>
             <button onClick = {()=>{if(count>0){setCount(count -1)}}}>Decrease</button>
