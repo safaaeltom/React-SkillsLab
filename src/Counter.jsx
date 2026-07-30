5ae53138acd231c5e79958db0b5114d9162ca6a9
Safaa <safaaeltom@gmail.com>
2026-07-30 10:21:07 +0300

    feat: add conditional progress messages

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,12 +5,15 @@ const Counter = () => {
 
     return ( 
         <div>
-            <h1>{count}</h1>
+            
+            <h1>{count*10}%</h1>
             <button onClick = {()=>{if(count<10){setCount(count +1)}}}>Increase</button>
             <button onClick = {()=>{if(count>0){setCount(count -1)}}}>Decrease</button>
             <button onClick = {()=>setCount(0)}>Reset</button>
 
-            {count===5 && <h3>Half way!</h3>}
+            {count<4 && <h3>Just started!</h3>}
+            {count>3 && count<8 && <h3>Making progress!</h3>}
+            {count>7 && count<10 && <h3>Almost there!</h3>}
             {count===10 && <h3>Maximum reached!</h3>}
         </div>
      );
