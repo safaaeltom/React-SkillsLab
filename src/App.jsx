9b7841cc5d61431d10dbe55e6ed961bbf4d97979
Safaa <safaaeltom@gmail.com>
2026-08-03 12:28:28 +0300

    feat: create initial todo list interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Counter from'./components/counter/Counter';
+import Counter from './components/counter/Counter';
+import TodoList from './components/todoList/TodoList';
 
 function App() {
   
   return (
     <div>
       <h1>React Skills</h1>
-      < Counter />
+      {/* < Counter /> */}
+      < TodoList />
     </div>
   )
 }
---
+++ src/components/todoList/TodoList.jsx
@@ -0,0 +1,11 @@
+const TodoList = () => {
+    return ( 
+        <div>
+            <h2>My Todo List</h2>
+            <input type="text" name="text" placeholder= "Enter a task"/>
+            <button>Add</button>
+        </div>
+     );
+}
+ 
+export default TodoList;
