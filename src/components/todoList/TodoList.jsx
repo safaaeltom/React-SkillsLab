e46de87313f1ed54114110f85538b1e92223c4a8
Safaa <safaaeltom@gmail.com>
2026-08-06 10:46:19 +0300

    feat: add todo deletion functionality

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -4,13 +4,20 @@ const TodoList = () => {
     const [input, setInput] = useState("");
     const [todo, setTodo] = useState([]);
 
-    const handleAdd = () =>{
+    const handleAdd = () => {
         const trimmedInput= input.trim();
 
         if(trimmedInput.length > 0) {
                 setTodo([...todo, trimmedInput])
                 setInput("")
                 }};
+
+    const handleDelete = (taskToDelete) => {
+        const updatedTodo = todo.filter((task) => task !== taskToDelete);
+        setTodo(updatedTodo);
+
+    }
+
     return ( 
         <div>
             <h2>My Todo List</h2>
@@ -29,7 +36,11 @@ const TodoList = () => {
         <ul>
            {todo.map((task) => {
                 return (
-                    <li key={task}>{task}</li>
+                    <li key={task}>
+                        {task}
+                        <button onClick={()=>handleDelete(task)}
+                         >Delete</button>
+                    </li>
                 );
               })
             }
