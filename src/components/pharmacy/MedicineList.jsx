e20cb12f3a194fe7a76aea9b8854a79314929461
Safaa <safaaeltom@gmail.com>
2026-08-21 20:58:47 +0300

    feat: implement medicine editing with controlled input, and save functionality

--- src/components/pharmacy/MedicineList.jsx
+++ src/components/pharmacy/MedicineList.jsx
@@ -51,27 +51,42 @@ const MedicineList = () => {
         );
     };
 
+    const [editingMedicineId, setEditingMedicineId] = useState(null);
+    const [input, setInput] = useState("");
+
     return ( 
         <div className="list">
             <h2>Medicine List</h2>
             <ul >
                 {medicines.map((medicine)=>(
-                    <li key={medicine.id}>
-                        <span className="name">{medicine.name}</span>
-                        <span className="quantity">{medicine.quantity}</span>
-                        <button onClick={()=>increaseMedicine(medicine.id)}>
-                            +
-                        </button>
-                        <button onClick={()=>decreaseMedicine(medicine.id)}>
-                            -
-                        </button>
-                        <button onClick={()=>deleteMedicine(medicine.id)}>
-                            Delete
-                        </button>
-                        <button onClick={()=>editMedicine(medicine.id)}>
-                            Edit
-                        </button>
-                        
+                <li key={medicine.id}>
+                    {medicine.id === editingMedicineId ? 
+                    (<input 
+                    value={input} 
+                    type= "text" 
+                    onChange={(e)=>setInput(e.target.value)}
+                    />):(
+                    <span className="name">{medicine.name}</span>)}
+                    <span className="quantity">{medicine.quantity}</span>
+                    <button onClick={()=>increaseMedicine(medicine.id)}>
+                        +
+                    </button>
+                    <button onClick={()=>decreaseMedicine(medicine.id)}>
+                         -
+                    </button>
+                    <button onClick={()=>deleteMedicine(medicine.id)}>
+                        Delete
+                    </button>
+                    <button onClick={()=>{
+                        if(medicine.id===editingMedicineId){
+                            editMedicine(editingMedicineId, input);
+                            setEditingMedicineId(null)
+                        }else{
+                            setEditingMedicineId(medicine.id);
+                            setInput(medicine.name)}
+                        }}>
+                        {medicine.id===editingMedicineId ? "Save" : "Edit"}    
+                        </button> 
                     </li>
                 ))}
             </ul>
