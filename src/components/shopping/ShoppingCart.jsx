1873f8a9f5a5bd228bd9d3d0ac01121614e46ff9
Safaa <safaaeltom@gmail.com>
2026-08-24 23:05:01 +0300

    Add product creation and form submission handling

--- src/components/shopping/ShoppingCart.jsx
+++ src/components/shopping/ShoppingCart.jsx
@@ -2,15 +2,26 @@ import {useState} from 'react';
 
 const ShoppingCart = () => {
     const [products, setProducts] = useState([
-        {id:1, name: "mobile ", type: "redmi", quantity: 2},
-        {id:2, name: "headset ", type: "bluetooth", quantity: 7},
-        {id:3, name: "laptop ", type: "toshipa", quantity: 20}
+        {id:1, name: "mobile ", type: "redmi ", quantity: 2},
+        {id:2, name: "headset ", type: "bluetooth ", quantity: 7},
+        {id:3, name: "laptop ", type: "toshipa ", quantity: 20}
     ])
 
     const [productName, setProductName] = useState("");
     const [productType, setProductType] = useState("");
     const [productQuantity, setProductQuantity] = useState("");
 
+    const addProduct = (productName, productType, productQuantity) => {
+        if(productName.trim()==="" || productType.trim()==="" || productQuantity<1){
+        return;
+        }
+        setProducts([...products, {
+            id: products.length + 1,
+            name: productName,
+            type: productType,
+            quantity: productQuantity
+        }])
+    };
 
     return ( 
         <div className="shopping-cart">
@@ -20,10 +31,16 @@ const ShoppingCart = () => {
                     <li key={product.id}>
                         <span className="name">{product.name}</span>
                         <span className="type">{product.type}</span>
+                        <span className="quantity">{product.quantity}</span>
+
                     </li>
                 ))}
             </ul>
-            <div>
+            <form onSubmit={(e)=>{
+                e.preventDefault();
+                addProduct(productName, productType, productQuantity)
+
+            }}>
                 <label htmlFor="product-name">Product</label>
                 <input type="text" value={productName}
                 placeholder="Enter name"
@@ -39,10 +56,10 @@ const ShoppingCart = () => {
                 placeholder="Enter quantity"
                 onChange={(e)=>setProductQuantity(Number(e.target.value))}
                 /><br/>
-                <button>Add Products</button>
-            </div>
+                <button type="submit">Add Products</button>
+            </form>
         </div>
      );
 }
- 
+
 export default ShoppingCart;
